797cbdbe58dec4929e26325a5bdfdc55056b0d6c
Kim YongRae <kyr9389@naver.com>
2020-05-18 22:26:03 +0900

    splash page added
    Signed-off-by: Kim YongRae <kyr9389@naver.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Headline">
+          <p>Front-End Developer</p>
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="#about"
           rel="noopener noreferrer"
         >
-          Learn React
+          view more
         </a>
       </header>
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
