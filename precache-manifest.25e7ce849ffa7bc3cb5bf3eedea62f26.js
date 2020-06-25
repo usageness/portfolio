b3de3677ed2cb17f33f9e6b88a10da946728ec61
yongrae <kyr9389@naver.com>
2020-06-25 22:02:53 +0900

    Updates

---
+++ precache-manifest.25e7ce849ffa7bc3cb5bf3eedea62f26.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce98a7b6f7bd0b9143831feb2c61572",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e1899a5e45c8757921a5",
+    "url": "/portfolio/static/css/main.59698fd1.chunk.css"
+  },
+  {
+    "revision": "473c8b62de6d974ab615",
+    "url": "/portfolio/static/js/2.3c70d403.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.3c70d403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1899a5e45c8757921a5",
+    "url": "/portfolio/static/js/main.a807c866.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "ec36395fa4c6ede37a46a2326a27d252",
+    "url": "/portfolio/static/media/icon_express.ec36395f.png"
+  },
+  {
+    "revision": "f9860a0aac12383d91796bfa3ba6ca04",
+    "url": "/portfolio/static/media/icon_html.f9860a0a.png"
+  },
+  {
+    "revision": "edada55e2146ce59d2e99d41f45a020d",
+    "url": "/portfolio/static/media/icon_js.edada55e.png"
+  },
+  {
+    "revision": "f347a266fdca7ab6e46d25582e31ca3c",
+    "url": "/portfolio/static/media/icon_node.f347a266.png"
+  },
+  {
+    "revision": "0892053f0eab2c88fa05b978162fe0fc",
+    "url": "/portfolio/static/media/icon_react.0892053f.png"
+  },
+  {
+    "revision": "80a2b33f563df54c4afab9f62eb999eb",
+    "url": "/portfolio/static/media/icon_vue.80a2b33f.png"
+  },
+  {
+    "revision": "a94d868ddc8984b4a24d8d1a4635372d",
+    "url": "/portfolio/static/media/ppic.a94d868d.jpg"
+  },
+  {
+    "revision": "a9b4a49b9e325a6c1610c55d37c40548",
+    "url": "/portfolio/static/media/project_logo_mapleitem.a9b4a49b.png"
+  },
+  {
+    "revision": "0405844a1a9b92317eecbd6569ea9bc1",
+    "url": "/portfolio/static/media/project_logo_starrynight.0405844a.png"
+  }
+]);
